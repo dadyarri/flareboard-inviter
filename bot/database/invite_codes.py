1b57d3da749850ea0e464034a962f9747f8e9b98
Daniil <dadyarri@gmail.com>
2021-11-10 16:49:08 +0300

    feat(database): add util to work with invite codes

---
+++ bot/database/invite_codes.py
@@ -0,0 +1,29 @@
+from tortoise.transactions import in_transaction
+
+from bot.database.models import InviteCode
+
+
+def get_invite_code():
+    """
+    Получает первый не использованный код приглашения
+
+    Returns:
+        InviteCode: код приглашения
+    """
+    async with in_transaction():
+        return InviteCode.filter(activated_by=None).first()
+
+
+def invalidate_invite_code(code: InviteCode, tg_id: int):
+    """
+    Помечает код приглашения невалидным
+
+    Args:
+        code: Код приглашения
+        tg_id: ИД пользователя в Telegram
+
+    Returns:
+        InviteCode: код приглашения
+    """
+    async with in_transaction():
+        return await InviteCode.filter(id=code.id).update(activated_by=tg_id)

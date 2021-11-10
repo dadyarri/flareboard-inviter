96df4b97cba76f4910684fa6ff01dfa398a81245
Daniil <dadyarri@gmail.com>
2021-11-10 20:26:58 +0300

    feat(bot): add generating codes

--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,13 @@
 import asyncio
 import logging
+import random
 
 from aiogram import Bot, Dispatcher, types
-from aiogram.dispatcher.filters import CommandStart
+from aiogram.dispatcher.filters import CommandStart, Command
 from aiogram.utils import executor
 
 from bot.database.core.init import init_db_connection
+from bot.database.models import InviteCode
 from database.invite_codes import get_invite_code, user_got_code, invalidate_invite_code
 from database.utils import get_tg_token
 
@@ -27,6 +29,13 @@ async def _get_token(message: types.Message):
             await invalidate_invite_code(code, message.from_user.id)
         else:
             await message.answer("Коды закончились :(")
+
+
+@dp.message_handler(Command("insert"))
+async def _add_codes(message: types.Message):
+    for _ in range(10):
+        await InviteCode.create(code=str(random.getrandbits(4)))
+    await message.answer("Коды сгенерированы")
 
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(init_db_connection())

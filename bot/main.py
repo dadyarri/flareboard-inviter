a88164af2a836f8fe134bfbd39f3bf62e08f4688
Daniil <dadyarri@gmail.com>
2021-11-25 20:23:39 +0300

    feat: add creating entries in database by sending file

--- bot/main.py
+++ bot/main.py
@@ -6,6 +6,7 @@ from aiogram.dispatcher.filters import CommandStart
 from aiogram.utils import executor
 
 from bot.database.core.init import init_db_connection
+from bot.database.models import InviteCode
 from database.invite_codes import get_invite_code, user_got_code, invalidate_invite_code
 from database.utils import get_tg_token
 
@@ -27,6 +28,20 @@ async def _get_token(message: types.Message):
             await invalidate_invite_code(code, message.from_user.id)
         else:
             await message.answer("Коды закончились :(")
+
+
+@dp.message_handler(content_types=types.ContentTypes.DOCUMENT)
+async def _upload_codes(message: types.Message):
+    file = await bot.get_file(message.document.file_id)
+    await bot.download_file(file.file_path, "invites.txt")
+
+    with open("invites.txt", "r") as f:
+        codes = map(lambda line: line.replace("\n", ""), f.readlines())
+
+    for code in codes:
+        await InviteCode.create(code=code)
+
+    await message.answer("Файл загружен")
 
 
 if __name__ == '__main__':
